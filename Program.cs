4afac8b49e9ac10c9c7bd455c3638a9bb151db1d
Alextor21511 <alextormon21511@gmail.com>
2021-11-06 10:46:36 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace producer_master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ productor/producer.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using System;
+//using Microsoft.Extensions.Configuration;
+using System.Net;
+
+class Producer
+{
+    static void Main()
+    {
+        var config = new ProducerConfig
+        {
+            
+            BootstrapServers = "localhost:9092",
+            
+        };
+        
+        Action<DeliveryReport<Null, string>> handler = r =>
+        Console.WriteLine(!r.Error.IsError
+        ? $"Delivered message to {r.TopicPartitionOffset}"
+        : $"Delivery Error: {r.Error.Reason}");
+
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            String texto;
+            int numProduced=0;
+            do
+            {
+                Console.WriteLine("Introduzca un texto (escribe \"SALIR\" para finalizar)");
+                texto = Console.ReadLine();
+                if(texto != "SALIR")
+                {
+                    producer.Produce("sd-events", new Message<Null, string> { Value = texto });
+                    numProduced++;
+                }
+            } while (texto != "SALIR");  
+            producer.Flush(TimeSpan.FromSeconds(10));
+            Console.WriteLine($"{numProduced} mensajes han sido producidos para el topic sd-events");
+        }
+    }
+}
